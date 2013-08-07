ba15067ae31e28a9aca2e0ac679d8ba1e4a916ca
gfsincere <gfsincere@gmail.com>
2013-08-07 00:12:31 -0500

    Update B663cpmigrate.sh

--- B663cpmigrate.sh
+++ B663cpmigrate.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 for i in `cat users.txt`; do /scripts/pkgacct $i; done
 echo "Account backups done" > email.txt
-mail -vv -s "Account backups done" greg.thomas@gmail.com < email.txt
+mail -vv -s "Account backups done" gregorie.thomas@gmail.com < email.txt
