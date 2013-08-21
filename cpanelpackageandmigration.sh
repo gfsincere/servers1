efd3dc6a2caf43b91280548ff6e0d75f947d86ec
gfsincere <gfsincere@gmail.com>
2013-08-21 03:50:11 -0500

    Update cpanelpackageandmigration.sh
    For when its just one account

--- cpanelpackageandmigration.sh
+++ cpanelpackageandmigration.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
-ls /var/cpanel/users/ > migration.txt
-tail -n +3 migration.txt > users.txt
+#ls /var/cpanel/users/ > migration.txt
+screen -d -m -S "cpantrans"
+#tail -n +3 migration.txt > users.txt
 for i in `cat users.txt`; do /scripts/pkgacct --skipdb $i; done
 mysql -u root -e "show databases"; grep -v Database; while read i; do mysqldump $i > /root/SQL/$i.sql; done
 php -v >> /root/INFO
