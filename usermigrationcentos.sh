c05068ea40a628fcb32334ce5d02ccef53215c44
gfsincere <gfsincere@gmail.com>
2014-04-20 14:24:52 -0500

    first try
    DAHHHH

---
+++ usermigrationcentos.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+mkdir /root/move/
+export UGIDLIMIT=500
+awk -v LIMIT=$UGIDLIMIT -F: '($3>=LIMIT) && ($3!=65534)' /etc/passwd > /root/move/passwd.mig
+awk -v LIMIT=$UGIDLIMIT -F: '($3>=LIMIT) && ($3!=65534)' /etc/group > /root/move/group.mig
+awk -v LIMIT=$UGIDLIMIT -F: '($3>=LIMIT) && ($3!=65534) {print $1}' /etc/passwd | tee - |egrep -f - /etc/shadow > /root/move/shadow.mig
+cp /etc/gshadow /root/move/gshadow.mig
+tar -zcvpf /root/move/home.tar.gz /home
+tar -zcvpf /root/move/mail.tar.gz /var/spool/mail
+scp -r /root/move/* root@162.243.100.238:/backup
